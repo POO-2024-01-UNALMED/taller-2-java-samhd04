587ade728f5342a230279dc740229589f22461b5
samhd04 <shernandezdu@unal.edu.co>
2024-02-15 21:45:31 -0500

    Creación de clases
    Primer prueba del código.

---
+++ src/main/java/Auto.java
@@ -0,0 +1,33 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int contador = 0;
+		
+		for(int i = 0; i < asientos.length; i++) {
+			if(asientos[i] != null) {
+				contador = contador + 1;
+			}
+		}
+		
+		return contador;
+	}
+	
+	String verificarIntegridad() {
+		for(int i = 0; i < asientos.length; i++) {
+			if(asientos[i].registro != this.registro || asientos[i].registro != 			motor.registro) {
+				return "Las piezas no son originales";
+			}
+		}
+		
+		return "Auto original";
+	}
+}
